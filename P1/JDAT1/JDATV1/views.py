85dcbcea725c46b0e8bd7879ed950723def7858a
Contreras926 <edgardavidcontreras19@gmail.com>
2025-10-29 01:07:16 -0500

    Antes de recrear entorno virtual

--- P1/JDAT1/JDATV1/views.py
+++ P1/JDAT1/JDATV1/views.py
@@ -3,6 +3,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def inicio(request):
-    return HttpResponse("<h1>Sapo</h1>")
+    return render (request, 'paginas/inicio.html') 
 def nosotros (request):
     return render (request, 'paginas/nosotros.html')     
+def productos (request):
+    return render (request, 'productos/index.html')
+def crear_producto (request):
+    return render (request, 'productos/crear.html')
+def editar_producto (request):
+    return render (request, 'productos/editar.html')
