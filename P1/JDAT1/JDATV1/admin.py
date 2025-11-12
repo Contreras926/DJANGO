6aafd505d4465c88e9ede5c67215d177a2e92b52
JPARDOM2006 <jpardomosquera@gmail.com>
2025-11-12 13:29:30 -0500

    login de prueba

--- P1/JDAT1/JDATV1/admin.py
+++ P1/JDAT1/JDATV1/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
-from .models import Producto
-# Register your models here.
-admin.site.register(Producto)
+from .models import Producto, Venta
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ['nombreProducto', 'categoria', 'preciolmitario', 'stockActual', 'stockMinimo']  # ← MANTIENE preciolmitario
+    list_filter = ['categoria']
+    search_fields = ['nombreProducto']
+
+@admin.register(Venta)
+class VentaAdmin(admin.ModelAdmin):
+    list_display = ['producto', 'cantidad', 'precio_venta', 'fecha_venta']
+    list_filter = ['fecha_venta', 'producto__categoria']
+    date_hierarchy = 'fecha_venta'
